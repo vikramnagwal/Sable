93ddb5e656ed01ef3954491ec74c5f10a9f7973f
Vikram Singh <nagwalanshu@gmail.com>
2026-01-28 21:23:13 +0530

    dev: auth added

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "@/components/ui/sign-in";
+
+export default function RegisterPage() {
+    return (
+        <div>
+            <h1>Register</h1>
+            <SignIn />
+        </div>
+    )
+}
--- app/(homepage)/components/header.tsx
+++ app/(homepage)/components/header.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-4 px-3">
+    <header className="flex items-center justify-between py-4 px-4 sm:px:-6 lg:px-8">
       <h1 className="text-xl">Sab/e</h1>
-      <button className="px-2 py-1 text-sm bg-neutral-200 text-black font-semibold cursor-pointer">
+      <Link href={"/register"} className="px-2 py-1 text-sm bg-neutral-200 text-black font-semibold cursor-pointer">
         log in
-      </button>
+      </Link>
     </header>
   );
 }
